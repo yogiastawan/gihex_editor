c9a9525a01e3950487baf3490fd6070551653aa3
Yogi Astawan <yogi.astawan@gmail.com>
2024-11-04 03:10:17 +0800

    separate primary (main) menu

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,3 +1,2 @@
 pub mod application;
-pub mod new_popover;
 pub mod window;
--- src/ui/window.rs
+++ src/ui/window.rs
@@ -1,12 +1,13 @@
+use std::borrow::Borrow;
+
 use adw::subclass::prelude::*;
+use gtk::gio::MenuModel;
 use gtk::prelude::*;
 use gtk::{gio, glib};
 
 mod imp {
 
-    use gtk::Label;
-
-    use crate::ui::new_popover::GihexNewPopover;
+    use gtk::MenuButton;
 
     use super::*;
 
@@ -15,8 +16,8 @@ mod imp {
     pub struct GihexWindow {
         // #[template_child]
         // pub label: TemplateChild<Label>,
-        // #[template_child]
-        // pub new_menu_popover: TemplateChild<GihexNewPopover>,
+        #[template_child]
+        pub primary_menu: TemplateChild<MenuButton>,
     }
 
     #[glib::object_subclass]
@@ -27,7 +28,7 @@ mod imp {
 
         fn class_init(klass: &mut Self::Class) {
             // register popover
-            GihexNewPopover::ensure_type();
+            // GihexNewPopover::ensure_type();
             klass.bind_template();
         }
 
@@ -36,7 +37,12 @@ mod imp {
         }
     }
 
-    impl ObjectImpl for GihexWindow {}
+    impl ObjectImpl for GihexWindow {
+        fn constructed(&self) {
+            self.parent_constructed();
+            self.obj().setup_menu();
+        }
+    }
     impl WidgetImpl for GihexWindow {}
 
     impl WindowImpl for GihexWindow {}
@@ -55,5 +61,13 @@ glib::wrapper! {
 impl GihexWindow {
     pub fn new<APP: IsA<gtk::Application>>(app: &APP) -> Self {
         glib::Object::builder().property("application", app).build()
+    }
+
+    fn setup_menu(&self) {
+        let builder = gtk::Builder::from_resource("/com/gihex/editor/ui/xml/main_menu.ui");
+        let main_menu = builder
+            .object::<MenuModel>("primary_menu_model")
+            .expect("Cannot get primary_menu_model");
+        self.imp().primary_menu.set_menu_model(Some(&main_menu));
     }
 }
